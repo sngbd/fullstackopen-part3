393d832ca273b20ef18b11123f3d604ce4474c03
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-07-22 13:07:46 +0700

    3.2

--- phonebook/index.js
+++ phonebook/index.js
@@ -30,6 +30,11 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  datetime = new Date()
+  response.send(`<p>Phonebook has info for ${persons.length} people</p> ${""+datetime}`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}/`)
