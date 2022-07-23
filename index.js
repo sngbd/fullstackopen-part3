6308f4427418c3374ac24a864c4236c784a88deb
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-07-23 22:45:21 +0700

    3.17

--- index.js
+++ index.js
@@ -82,6 +82,21 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+        response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+  })
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}/`)
