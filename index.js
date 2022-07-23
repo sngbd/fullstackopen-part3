1239fdc137b3a0c8c68a3c774aeb773e80feddc5
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-07-23 14:24:16 +0700

    3.15

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
     if (person) {
       response.json(person.toJSON())
@@ -36,7 +36,7 @@ app.get('/api/persons/:id', (request, response) => {
       response.status(404).end()
     }
   })
-  .catch(error => console.log(error))
+  .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -86,3 +86,16 @@ const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}/`)
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
