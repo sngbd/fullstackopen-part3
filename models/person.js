c4c4f3db7a02ff28e79ae2bfc6288e789bb1173f
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-07-24 14:24:40 +0700

    3.20

--- models/person.js
+++ models/person.js
@@ -20,6 +20,12 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
+    validate: {
+      validator: function(v) {
+        return (/^\d{2}-\d{6,}/.test(v) || /\d{8,}/.test(v) || /^\d{3}-\d{5,}/.test(v));
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
     required: true
   }
 })
